11023b45f8c516d099b1636c70b7f3c620ac3c62
Nagendrakumar Mavuri <Nagendrakumarm@gmail.com>
2026-01-09 08:50:47 -0500

    updated to mobile friendly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,26 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    NgIf
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Budget Monitor';
+  isMobile = window.innerWidth < 768;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  ngOnInit() {
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth < 768;
+    });
+  }
+
+  closeIfMobile() {
+    if (this.isMobile) {
+      this.sidenav.close();
+    }
+  }
 }
