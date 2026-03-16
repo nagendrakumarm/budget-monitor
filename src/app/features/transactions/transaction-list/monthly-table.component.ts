9e9a44b0eb4f223501cb9cce77e1f837732274ef
Nagendrakumar Mavuri <Nagendrakumarm@gmail.com>
2026-03-15 21:54:59 -0400

    fixed monthly summary

--- src/app/features/transactions/transaction-list/monthly-table.component.ts
+++ src/app/features/transactions/transaction-list/monthly-table.component.ts
@@ -11,10 +11,34 @@ import { MonthlySummary } from '../../../core/models/transaction.model';
 })
 export class MonthlyTableComponent implements OnInit {
   summaries: MonthlySummary[] = [];
+  grandTotals: any;
 
   constructor(private trasactionService: TransactionService) {}
 
   async ngOnInit() {
     this.summaries = await this.trasactionService.getMonthlySummary();
+    this.calculateGrandTotals();
   }
+
+  calculateGrandTotals() {
+    const totals = {
+      month: 'TOTAL',
+      income: 0,
+      total: 0,
+      needs: 0,
+      wants: 0,
+      investments: 0
+    };
+
+    for (const row of this.summaries) {
+      totals.income += row.income || 0;
+      totals.total += row.total || 0;
+      totals.needs += row.needs || 0;
+      totals.wants += row.wants || 0;
+      totals.investments += row.investments || 0;
+    }
+
+    this.grandTotals = totals;
+  }
+
 }
